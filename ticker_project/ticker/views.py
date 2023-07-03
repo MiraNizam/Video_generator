89bb4d8ae52014f058202826741242b2b4aca58a
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2023-07-03 22:37:57 +0300

    Add downloading file

--- ticker_project/ticker/views.py
+++ ticker_project/ticker/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
-
+import os
 from .run_text import generate_ticker
 from .models import UserInquiry
+from django.http import FileResponse
 
 
-# Create your views here.
 def home(request):
     return render(request, "index.html")
 
 
-def runtext(request):
+def download_video(request):
     text = request.GET.get("text")
-    print(type(text))
     if text is None:
         return render(request, "runtext.html")
     else:
-        UserInquiry.objects.create(input_text = text)
-        generate_ticker(text)
-        return render(request, "runtext.html")
+        file_path = generate_ticker(text)
+        UserInquiry.objects.create(input_text=text)
+        response = FileResponse(open(file_path, "rb"), content_type="video/mp4")
+        response['Content-Disposition'] = 'attachment; filename="video.mp4"'
+        return response
